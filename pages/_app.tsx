f03d66158445134180a9ddebb9657cc4b9244cea
vinicenter <viniemidiobosi@gmail.com>
2025-06-15 23:52:01 -0300

    feat: add edge runtime support to the application

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,6 +10,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "../assets/globals.css";
 
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -19,6 +20,8 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
+
+export const runtime = "edge";
 
 export const metadata: Metadata = {
   title: "Vista-se Estilo e Conforto",
